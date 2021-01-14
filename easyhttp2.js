f493febafe4ebbea52b35da0e41e7ad8f8cd7087
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2021-01-14 19:16:18 +1300

    convert get request to aysnc/await

--- easyhttp2.js
+++ easyhttp2.js
@@ -2,23 +2,22 @@
  * EasyHTTP Library
  * Library for making HTTP requests
  *
- * @version 2.0.0
- * @author  Brad Traversy
+ * @version 3.0.0
+ * @author  Brad Traversy - David Steel
  * @license MIT
  *
  **/
 
+// Convert to async/await
+
  class EasyHTTP {
    
   // Make an HTTP GET Request 
-  get(url) {
-    return new Promise((resolve, reject) => {
-      fetch(url)
-      .then(res => res.json())
-      .then(data => resolve(data))
-      .catch(err => reject(err));
-    });
-  }
+  async get(url) {
+      const response = await fetch(url)
+      const resData = await response.json()
+      return resData
+    };
 
   // Make an HTTP POST Request
   post(url, data) {
